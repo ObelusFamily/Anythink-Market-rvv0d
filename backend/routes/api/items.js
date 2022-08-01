06f833b5844325289b715d5c81518b3d4908607e
maruthi-18 <marurohi@gmail.com>
2022-08-02 02:57:01 +0530

    bug2 fix

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -147,7 +147,9 @@ router.post("/", auth.required, function(req, res, next) {
       var item = new Item(req.body.item);
 
       item.seller = user;
-      item.image?item.image:"placeholder.png";
+      if(item.image === ""){
+        item.image = "placeholder.png"
+      }
       return item.save().then(function() {
         sendEvent('item_created', { item: req.body.item })
         return res.json({ item: item.toJSONFor(user) });
